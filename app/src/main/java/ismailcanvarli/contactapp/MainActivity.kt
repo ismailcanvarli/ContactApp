49e316d6f09d9ea58babce4cf8c0ac77de56cff2
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-12-18 16:11:41 +0300

    Add contact app nav graph to main activity

--- app/src/main/java/ismailcanvarli/contactapp/MainActivity.kt
+++ app/src/main/java/ismailcanvarli/contactapp/MainActivity.kt
@@ -3,12 +3,7 @@ package ismailcanvarli.contactapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import ismailcanvarli.contactapp.navigation.NavGraph
 import ismailcanvarli.contactapp.ui.theme.ContactAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,19 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ContactAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android", modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NavGraph()
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!", modifier = modifier
-    )
 }
