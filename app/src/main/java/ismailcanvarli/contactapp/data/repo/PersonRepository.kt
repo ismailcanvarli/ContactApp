c56b588a56c55e65697f633824464c43b8ed0c86
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-12-24 20:30:00 +0300

    feat: Enhance PersonRepository with new data methods

--- app/src/main/java/ismailcanvarli/contactapp/data/repo/PersonRepository.kt
+++ app/src/main/java/ismailcanvarli/contactapp/data/repo/PersonRepository.kt
@@ -3,7 +3,21 @@
 package ismailcanvarli.contactapp.data.repo
 
 import ismailcanvarli.contactapp.data.datasource.PersonDataSource
+import ismailcanvarli.contactapp.data.entity.Person
 
 class PersonRepository {
     var personDataSource = PersonDataSource()
+
+    suspend fun uploadPerson(): List<Person> = personDataSource.uploadPerson()
+
+    suspend fun save(personName: String, personPhone: String) =
+        personDataSource.save(personName, personPhone)
+
+    suspend fun updatePerson(personId: Int, personName: String, personPhone: String) =
+        personDataSource.updatePerson(personId, personName, personPhone)
+
+    suspend fun delete(personId: Int) = personDataSource.delete(personId)
+
+    suspend fun searchPerson(searchWord: String): List<Person> =
+        personDataSource.searchPerson(searchWord)
 }
