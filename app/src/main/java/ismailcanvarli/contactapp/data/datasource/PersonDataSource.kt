78f8e439b85b4b020373e12e9cea9b783b10fb61
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-12-26 22:14:30 +0300

    Update: PersonDataSource for data handling improvements

--- app/src/main/java/ismailcanvarli/contactapp/data/datasource/PersonDataSource.kt
+++ app/src/main/java/ismailcanvarli/contactapp/data/datasource/PersonDataSource.kt
@@ -2,42 +2,33 @@
 
 package ismailcanvarli.contactapp.data.datasource
 
-import android.util.Log
 import ismailcanvarli.contactapp.data.entity.Person
+import ismailcanvarli.contactapp.retrofit.PersonDao
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class PersonDataSource {
+class PersonDataSource(var personDao: PersonDao) {
     suspend fun save(personName: String, personPhone: String) {
-        Log.e("Person Save", "$personName - $personPhone")
+        personDao.savePerson(personName, personPhone)
     }
 
     suspend fun updatePerson(personId: Int, personName: String, personPhone: String) {
-        Log.e("Person Update", "$personId - $personName - $personPhone")
+        personDao.updatePerson(personId, personName, personPhone)
     }
 
     suspend fun delete(personId: Int) {
-        Log.e("Person Delete", personId.toString())
+        personDao.deletePerson(personId)
     }
 
     suspend fun uploadPerson(): List<Person> = withContext(Dispatchers.IO) {
-        val personList = ArrayList<Person>()
-        val p1 = Person(1, "Ahmet", "1111")
-        val p2 = Person(2, "Zeynep", "2222")
-        val p3 = Person(3, "Beyza", "3333")
-        personList.add(p1)
-        personList.add(p2)
-        personList.add(p3)
-
-        return@withContext personList
+        val response = personDao.allPersons()
+        // Gelen liste null olabilir; null kontrolü ekleyin
+        return@withContext response.persons ?: emptyList()
     }
 
     suspend fun searchPerson(searchWord: String): List<Person> = withContext(Dispatchers.IO) {
-        val personList = ArrayList<Person>()
-        val p1 = Person(1, "Ahmet", "1111")
-
-        personList.add(p1)
-
-        return@withContext personList
+        val response = personDao.searchPerson(searchWord)
+        // Gelen liste null olabilir; null kontrolü ekleyin
+        return@withContext response.persons ?: emptyList()
     }
 }
